4d88672f9cd8f0a862b762015528c46a5d4a118d
Alireza mahmoudi <ali.mahmoudi500544@gamil.com>
2021-05-29 20:41:50 +0430

    fixed bug

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 mongoose.connect("mongodb://localhost:27017/login");
 
 //send public folder
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 //api
 app.use("/", api);
