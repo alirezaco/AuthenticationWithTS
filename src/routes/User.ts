b74203009226fb644ed3108caeb77080fff448ff
Alireza mahmoudi <ali.mahmoudi500544@gamil.com>
2021-05-30 15:54:27 +0430

    api for user

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -11,16 +11,37 @@ router.post("/", (req, res, next) => {
   const newUser: UserFace = req.body;
 
   Users.createUser(newUser)
-    .then(() => res.send({}))
+    .then(() => res.send({ message: "New user created" }))
     .catch((err) => next(err));
 });
 
-//find user all user
+//get all user
 router.get("/", (req, res, next) => {
   Users.findAllUser()
     .then((users) => {
       res.send(users);
     })
+    .catch((err) => next(err));
+});
+
+//get User By id
+router.get("/:id", (req, res, next) => {
+  Users.findUserById(req.params.id)
+    .then((user) => res.send(user))
+    .catch((err) => next(err));
+});
+
+//delete user
+router.delete("/:id", (req, res, next) => {
+  Users.deleteUser(req.params.id)
+    .then(() => res.send({ message: "User deleted" }))
+    .catch((err) => next(err));
+});
+
+//Update User
+router.put("/:id", (req, res, next) => {
+  Users.updateUser(req.params.id, req.body)
+    .then((newUser) => res.send({ message: "User updated", newUser }))
     .catch((err) => next(err));
 });
 
