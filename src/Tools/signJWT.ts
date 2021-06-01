7c722825fd5389765a1cddd9aab85402e039aba7
Alireza mahmoudi <ali.mahmoudi500544@gamil.com>
2021-06-01 13:02:02 +0430

    sign JWT

---
+++ src/Tools/signJWT.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { UserFace } from "../classes/User";
+//config file
+import config from "../config/config";
+
+const signJWT = function(user: UserFace): Promise<string | undefined> {
+  return new Promise((resolve, reject) => {
+    try {
+      const token = jwt.sign(
+        {
+          user,
+        },
+        config.SERVER.token.secret,
+        {
+          expiresIn: config.SERVER.token.expireTime, // 1 day
+        }
+      );
+
+      resolve(token);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
